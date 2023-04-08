8f313bd78feaa9bd8a3f14ad90d83da70341a351
Abhishek Roy <100830257+abhishek-Roy22@users.noreply.github.com>
2023-04-08 10:45:33 +0000

    custom_error_page

---
+++ pages/404.js
@@ -0,0 +1,26 @@
+import Link from "next/link"
+
+const Error = () => {
+    return (
+        <div className="error">
+            <h1>404</h1>
+            <h2>Oooops! That page cannot be found :(</h2>
+            <p>Redirecting to the <Link href="/">Home Page</Link> for more Recipes goodness...</p>
+
+            <style jsx>{`
+                .error {
+                    background: #fff;
+                    padding: 30px;
+                    box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+                    transform: rotateZ(-1deg)
+                    text-align: center;
+                }
+                h1{
+                    font-size: 3em;
+                }
+            `}</style>
+        </div>
+    )
+}
+
+export default Error;
