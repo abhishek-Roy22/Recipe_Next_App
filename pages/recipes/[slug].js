834e8bd13d7959ba48f06f205fdc4861fb0ffbb2
Abhishek Roy <100830257+abhishek-Roy22@users.noreply.github.com>
2023-04-08 10:13:09 +0000

    fallback

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,5 +1,6 @@
 import { createClient } from "contentful";
 import Image from "next/image";
+import Skeleton from "../../components/Skeleton"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 const client = createClient({
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
@@ -37,6 +38,7 @@ export const getStaticProps = async ({ params }) => {
 };
 
 const RecipeDetails = ({ recipe }) => {
+  if(!recipe) return <Skeleton/>
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
   return (
